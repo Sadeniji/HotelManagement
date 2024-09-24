1c41c5b95058e64a29fa390678d1a2b1c071eb77
Saheed Adeniji <adeniji4success@gmail.com>
2024-09-24 17:31:52 +0100

    Add filter section to home and room page

---
+++ HotelManagement/Models/Public/FilterModel.cs
@@ -0,0 +1,35 @@
+﻿namespace HotelManagement.Models.Public;
+
+public class FilterModel
+{
+    public FilterModel()
+    {
+        
+    }
+    public static FilterModel GetFilterModel(DateOnly? checkInDate, DateOnly? checkOutDate, int? numberOfAdults, int? numberOfChildren)
+    {
+        return new FilterModel
+        {
+            CheckInDate = checkInDate,
+            CheckOutDate = checkOutDate,
+            NumberOfAdults = numberOfAdults,
+            NumberOfChildren = numberOfChildren
+        };
+
+    }
+
+    public DateOnly? CheckInDate { get; set; } //= DateOnly.FromDateTime(DateTime.Now);
+    public DateOnly? CheckOutDate { get; set; } //= DateOnly.FromDateTime(DateTime.Now.AddDays(1));
+
+    public int? NumberOfAdults { get; set; } = 0;
+    public int? NumberOfChildren { get; set; } = 0;
+
+    public IReadOnlyDictionary<string, object?> TDictionary() =>
+        new Dictionary<string, object?>()
+        {
+            [nameof(CheckInDate)] = CheckInDate,
+            [nameof(CheckOutDate)] = CheckOutDate,
+            [nameof(NumberOfAdults)] = NumberOfAdults,
+            [nameof(NumberOfChildren)] = NumberOfChildren
+        };
+}
--- HotelManagement/Services/Public/RoomsService.cs
+++ HotelManagement/Services/Public/RoomsService.cs
@@ -6,29 +6,41 @@ namespace HotelManagement.Services.Public;
 
 public interface IRoomsService
 {
-    Task<RoomTypeModel[]> GetRoomTypesAsync(int count = 0);
+    Task<RoomTypeModel[]> GetRoomTypesAsync(int count = 0, FilterModel? filter = null);
 }
 public class RoomsService(IDbContextFactory<ApplicationDbContext> contextFactory) : IRoomsService
 {
-    public async Task<RoomTypeModel[]> GetRoomTypesAsync(int count = 0)
+    public async Task<RoomTypeModel[]> GetRoomTypesAsync(int count = 0, FilterModel? filter = null)
     {
         await using var context = await contextFactory.CreateDbContextAsync();
 
-        var query = context.RoomTypes
-                                                      .Where(rt => rt.IsActive)
-                                                      .Select(rt => 
-                                                          new RoomTypeModel(
-                                                              rt.Id, 
-                                                              rt.Name,
-                                                              rt.Image,
-                                                              rt.Description, 
-                                                              rt.Price,
-                                                              rt.Amenities.Select(a => new RoomTypeAmenityModel(a.Amenity.Name, a.Amenity.Icon, a.Unit)).ToArray()));
+        var query = context.RoomTypes.Where(rt => rt.IsActive);
+
+        if (filter?.NumberOfAdults > 0)
+        {
+            query = query.Where(rt => rt.MaxAdults >= filter.NumberOfAdults);
+        }
+
+        if (filter?.NumberOfChildren > 0)
+        {
+            query = query.Where(rt => rt.MaxAdults >= filter.NumberOfChildren);
+        }
+
+        // Get the bookings for these checkin checkout dates
+        //check the available room types for the dates
 
         if (count > 0)
         {
             query = query.Take(count);
         }
-        return await query.ToArrayAsync();
+
+        return await query.Select(rt => new RoomTypeModel(
+                rt.Id,
+                rt.Name,
+                rt.Image,
+                rt.Description,
+                rt.Price,
+                rt.Amenities.Select(a => new RoomTypeAmenityModel(a.Amenity.Name, a.Amenity.Icon, a.Unit)).ToArray()))
+            .ToArrayAsync();
     }
 }
